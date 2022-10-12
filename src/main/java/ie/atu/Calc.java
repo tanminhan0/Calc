359700d43a39479b3daf1149d25037b9f179acb8
MIN HAN TAN <G00377211@atu.ie>
2022-10-12 09:44:25 +0100

    Added new multiply feature

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,23 @@
 package ie.atu;
 
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        multiple();
+    }
 
+
+    public static void multiple()
+    {
+        System.out.println("Please enter your first number : ");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number : ");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber * secondNumber;
+        System.out.println("The answer is " + total);
     }
 }
