93f0b9c83b12d98158bd337714561813396736ae
Conor Finnerty <G00383112@atu.ie>
2022-10-12 10:01:25 +0100

    added a new feature "del()"

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -22,4 +22,16 @@ public class Calc {
         System.out.println("The total is " + total);
 
     }
+    public static void del() {
+        System.out.println("Please enter the first number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter the second number");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total is " + total);
+
+    }
 }
