bfd8263b0c49a71f1dca2882d037128663f9c3ae
Conor Finnerty <G00383112@atu.ie>
2022-10-12 09:51:21 +0100

    added a new feature "add()"

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,25 @@
 package ie.atu;
 
+import org.w3c.dom.ls.LSInput;
+
+import java.util.Scanner;
+
 public class Calc {
     public static void main(String[] args) {
+        add();
+
+    }
+
+    public static void add() {
+        System.out.println("Please enter the first number");
+        Scanner inputs = new Scanner(System.in);
+        int firstNumber = inputs.nextInt();
+
+        System.out.println("Please enter the second number");
+        int secondNumber = inputs.nextInt();
+
+        int total = firstNumber + secondNumber;
+        System.out.println("The total is " + total);
 
     }
 }
