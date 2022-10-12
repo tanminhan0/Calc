31aa92e1ecebe4f8d4fd4133ed686823452e3d32
Aidan <G00370587@atu.ie>
2022-10-12 09:52:10 +0100

    added new method called delete

--- src/main/java/ie/atu/Calc.java
+++ src/main/java/ie/atu/Calc.java
@@ -1,7 +1,22 @@
 package ie.atu;
+import java.util.Scanner;
 
 public class Calc {
     public static void main(String[] args) {
 
+        delete();
+    }
+
+    public static void delete()
+    {
+        System.out.println("Please enter your first number:");
+        Scanner inputs = new Scanner(System.in);
+        int firstnumber = inputs.nextInt();
+
+        System.out.println("Please enter your second number:");
+        int secondnumber = inputs.nextInt();
+
+        int total = firstnumber - secondnumber;
+        System.out.println("The total is: " + total);
     }
 }
